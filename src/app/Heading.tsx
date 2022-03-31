6fb9e5d931d5d7daec7c2147d505765383c42a9b
Sparsh Paliwal <sparshpaliwal@wireless-10-104-105-112.umd.edu>
2022-03-31 00:11:01 -0400

    Update heading layout

--- src/app/Heading.tsx
+++ src/app/Heading.tsx
@@ -2,18 +2,16 @@
 import React from "react";
 import Steps from "./Steps";
 import Box, { BoxProps } from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Panel from "../components/Panel";
 
 const Heading: React.FC<BoxProps> = (props) => {
   return (
     <Box display="flex" justifyContent="space-between" width="100%" {...props}>
-      <Typography variant="h3" color="orange" width="128px">
-        A11y
-      </Typography>
-      <Steps width="100%" maxWidth="800px" paddingY="1em" activeStep={0} />
-      <Panel />
-      <div css={{ width: "128px" }} />
+      <div css={{ width: "10%" }}>
+        <img src="/logo.svg" alt="logo of FormA11y" />
+      </div>
+      <Steps width="80%" activeStep={2} />
+      <Panel width="10%" />
     </Box>
   );
 };
--- src/app/Steps.tsx
+++ src/app/Steps.tsx
@@ -1,32 +1,130 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import { styled } from "@mui/material/styles";
 import Box, { BoxProps } from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
+import Check from "@mui/icons-material/Check";
+import StepConnector, {
+  stepConnectorClasses,
+} from "@mui/material/StepConnector";
+import { StepIconProps } from "@mui/material/StepIcon";
+
+const STEPS = [
+  {
+    title: "Section",
+    description:
+      "Mark the area you want to fix first. We will go through the form in small chunks. Ensure that fields or groups (radioboxes) are completely included and not cut off in half .",
+  },
+  {
+    title: "Fields",
+    description:
+      "Ensure all form fields have a box and a field type present on them. If not, draw a box using the mouse and assign the field type.",
+  },
+  {
+    title: "Labels",
+    description:
+      "Ensure all form fields have a label associated to them. If not, select the field and use update label from the popup.",
+  },
+  {
+    title: "Groups",
+    description:
+      "Ensure the checkbox and radiobox are grouped properly and have group names. If not, you can select multiple boxes by dragging or Shift+Click and use popup menu to group fields. ",
+  },
+  {
+    title: "Tooltips",
+    description:
+      "Ensure these field descriptions (tooltips) are sufficient. If needed, add more information about the field using the edit button.",
+  },
+];
+
+// styled is a part of mui and is a recommendation for reusable style overrides.
+// Ref: https://mui.com/customization/how-to-customize/#2-reusable-style-overrides
+const Connector = styled(StepConnector)(() => ({
+  [`&.${stepConnectorClasses.alternativeLabel}`]: {
+    top: 10,
+    left: "calc(-50% + 16px)",
+    right: "calc(50% + 16px)",
+  },
+  [`&.${stepConnectorClasses.active}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: "#1473E6",
+    },
+  },
+  [`&.${stepConnectorClasses.completed}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: "#1473E6",
+    },
+  },
+  [`& .${stepConnectorClasses.line}`]: {
+    borderColor: "#eaeaf0",
+    borderTopWidth: 3,
+    borderRadius: 1,
+  },
+}));
+
+const StepIconRoot = styled("div")<{ ownerState: { active?: boolean } }>(
+  ({ theme, ownerState }) => ({
+    color: "#eaeaf0",
+    display: "flex",
+    height: 22,
+    alignItems: "center",
+    ...(ownerState.active && {
+      color: "#1473E6",
+    }),
+    "& .StepIcon-completedIcon": {
+      color: "#1473E6",
+      zIndex: 1,
+      fontSize: 18,
+    },
+    "& .StepIcon-circle": {
+      width: 8,
+      height: 8,
+      borderRadius: "50%",
+      backgroundColor: "currentColor",
+    },
+  })
+);
+
+function StepIcon(props: StepIconProps) {
+  const { active, completed, className } = props;
+
+  return (
+    <StepIconRoot ownerState={{ active }} className={className}>
+      {completed ? (
+        <Check className="StepIcon-completedIcon" />
+      ) : (
+        <div className="StepIcon-circle" />
+      )}
+    </StepIconRoot>
+  );
+}
 
 const Steps: React.FC<BoxProps & { activeStep: number }> = (props) => {
   const { activeStep, ...boxProps } = props;
+  //BUG: is not boxProps inline style ?
   return (
-    <Box {...boxProps}>
-      <Stepper activeStep={activeStep}>
-        <Step>
-          <StepLabel>Section</StepLabel>
-        </Step>
-        <Step>
-          <StepLabel>Fields</StepLabel>
-        </Step>
-        <Step>
-          <StepLabel>Labels</StepLabel>
-        </Step>
-        <Step>
-          <StepLabel>Groups</StepLabel>
-        </Step>
-        <Step>
-          <StepLabel>Tooltips</StepLabel>
-        </Step>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      width="100%"
+      {...boxProps}>
+      <Stepper
+        css={{ width: "600px" }}
+        alternativeLabel
+        activeStep={activeStep}
+        connector={<Connector />}>
+        {STEPS.map((step) => (
+          <Step key={step.title}>
+            <StepLabel StepIconComponent={StepIcon}>{step.title}</StepLabel>
+          </Step>
+        ))}
       </Stepper>
+      <div css={{ marginTop: "20px", fontWeight: "bold" }}>
+        {STEPS[activeStep].description}
+      </div>
     </Box>
   );
 };
-
 export default Steps;
