32e754a94a2c2f1c99a7652a72ba2500b43b796d
Sparsh Paliwal <sparshpailwal123@gmail.com>
2022-10-11 19:17:31 -0400

    Add tooltips to create and select tools

--- ui/src/app/ToolSelect.tsx
+++ ui/src/app/ToolSelect.tsx
@@ -6,7 +6,7 @@ import {
   DEFAULT_ACCESSIBLE_FORM,
 } from "./StoreProvider";
 import Box, { BoxProps } from "@mui/material/Box";
-import { IconButton } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 import color from "../components/color";
 import NearMeIcon from "@mui/icons-material/NearMe";
 import FormatShapesIcon from "@mui/icons-material/FormatShapes";
@@ -18,27 +18,30 @@ interface ToolButtonProps {
   toolName: TOOL;
   activeTool: string;
   children: React.ReactNode;
+  tooltip: string;
 }
 
 const ToolButton: React.FC<ToolButtonProps> = (props) => {
-  const { toolName, activeTool, children } = props;
+  const { toolName, activeTool, tooltip, children } = props;
   const dispatch = useDispatch();
   const isActiveColor =
     toolName === activeTool ? color.blue.medium : color.black;
   return (
-    <IconButton
-      onClick={() => dispatch({ type: "CHANGE_TOOL", payload: toolName })}
-      sx={{
-        "&:hover": {
-          color: color.blue.medium,
-          backgroundColor: color.blue.transparent,
-          borderRadius: "12px",
-        },
-        color: isActiveColor,
-        marginBottom: "8px",
-      }}>
-      {children}
-    </IconButton>
+    <Tooltip title={tooltip} placement="right">
+      <IconButton
+        onClick={() => dispatch({ type: "CHANGE_TOOL", payload: toolName })}
+        sx={{
+          "&:hover": {
+            color: color.blue.medium,
+            backgroundColor: color.blue.transparent,
+            borderRadius: "12px",
+          },
+          color: isActiveColor,
+          marginBottom: "8px",
+        }}>
+        {children}
+      </IconButton>
+    </Tooltip>
   );
 };
 
@@ -79,10 +82,16 @@ const ToolSelect: React.FC<BoxProps> = (props) => {
     case "FIELD_LAYER": {
       tools = (
         <>
-          <ToolButton activeTool={activeTool} toolName="SELECT">
+          <ToolButton
+            activeTool={activeTool}
+            toolName="SELECT"
+            tooltip="Select">
             <CursorIcon />
           </ToolButton>
-          <ToolButton activeTool={activeTool} toolName="CREATE">
+          <ToolButton
+            activeTool={activeTool}
+            toolName="CREATE"
+            tooltip="Create">
             <FormatShapesIcon />
           </ToolButton>
           <ResetButton />
@@ -93,7 +102,10 @@ const ToolSelect: React.FC<BoxProps> = (props) => {
     default: {
       tools = (
         <>
-          <ToolButton activeTool={activeTool} toolName="SELECT">
+          <ToolButton
+            activeTool={activeTool}
+            toolName="SELECT"
+            tooltip="Select">
             <CursorIcon />
           </ToolButton>
           <ResetButton />
